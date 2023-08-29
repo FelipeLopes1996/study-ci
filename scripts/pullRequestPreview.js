e597be805336538fc76b3c474f23cfa24aa72715
FelipeLopes1996 <felipel.lima1996@gmail.com>
2023-08-29 18:52:14 -0300

    updated

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -2,14 +2,12 @@ import { execSync } from "child_process";
 import fetch from "node-fetch";
 
 console.log("[DEPLOY_PREVIEW]: START");
-
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf8" });
 const outputLines = output.split("\n");
 const DEPLOY_URL = outputLines[outputLines.length - 1];
-console.log(output);
-
 console.log("[DEPLOY_PREVIEW]: END");
+
 console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 // ===================================
