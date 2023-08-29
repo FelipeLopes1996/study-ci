80b534d7368c542e9c9a3b2ea7831ec22bc648da
FelipeLopes1996 <felipel.lima1996@gmail.com>
2023-08-29 18:42:46 -0300

    updated

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,4 +1,5 @@
 import { execSync } from "child_process";
+import fetch from "node-fetch";
 
 console.log("[DEPLOY_PREVIEW]: START");
 
@@ -11,5 +12,43 @@ console.log(output);
 console.log("[DEPLOY_PREVIEW]: END");
 console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
-// console.log("[DEPLOY_PREVIEW]: START");
-// console.log("[DEPLOY_PREVIEW]: END");
+// ===================================
+// ===================================
+
+console.log("[GITHUB_COMMENT]: START");
+const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_PR_NUMBER } = process.env;
+const GH_COMMENT = `
+- Deploy URL: [${DEPLOY_URL}](${DEPLOY_URL})
+`;
+
+const defaultHeaders = {};
+defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
+defaultHeaders["accept"] =
+  "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
+defaultHeaders["content-type"] = "application/json";
+
+console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+
+fetch(
+  `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
+  {
+    headers: defaultHeaders,
+    method: "POST",
+    body: JSON.stringify({
+      body: GH_COMMENT,
+    }),
+    // eslint-disable-next-line prettier/prettier
+  }
+)
+  .then((response) => {
+    if (response.ok) return response.json();
+    throw new Error(response.statusText);
+  })
+  .catch((err) => {
+    console.log("[COMMENT_ON_GITHUB: ERROR]");
+    throw new Error(err);
+  })
+  .finally(() => {
+    console.log("[COMMENT_ON_GITHUB: END]");
+  });
